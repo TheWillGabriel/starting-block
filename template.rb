4b30c4fdc80137a8e6143722bb6dbe1df4023840
TheWillGabriel <will.gabriel@gmail.com>
2019-09-29 12:55:57 -0600

    Set Rails required version

---
+++ template.rb
@@ -0,0 +1,13 @@
+# Rails 6 application template
+# Usage: rails new [app_name] -m https://raw.githubusercontent.com/thewillgabriel/starting-block/master/template.rb -d postgresql
+
+RAILS_VERSION = ">= 6.0.0"
+
+def confirm_rails_version
+  required_version = Gem::Requirement.new(RAILS_VERSION)
+  current_version = Gem::Version.new(Rails::VERSION::STRING)
+  return if required_version.satisfied_by?(current_version)
+
+  puts "Please install Rails #{RAILS_VERSION} to use this template. Aborting."
+  exit 1
+end
